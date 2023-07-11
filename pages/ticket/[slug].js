8dac281d333461de677d8abc7d870a3b819091c2
Minh <t.hoangminh2411@gmail.com>
2023-07-11 15:12:25 +0700

    #Template builder +Update list +Update view +Update script view all

--- pages/ticket/[slug].js
+++ pages/ticket/[slug].js
@@ -1,22 +1,26 @@
 // @refresh reset
-import { headerHtml, headerScript, headerStyle, parseDataTicket } from "../configs/constants";
 
-export default function Home({html, css, script}) {
+import { headerHtml, headerScript, headerStyle, parseDataTicket, parseDataTicketList } from "../../configs/constants";
+
+
+export default function TicketViewPage({html, css, script, ticketId}) {
   return (
     <>
-     
+        Ticket ID: {ticketId}
        <div  dangerouslySetInnerHTML={{ __html: css + html + script  }}></div>
     </>
   )
 }
 
 export async function getServerSideProps(context) {
-  console.log("id",  context.params)
+  const {slug} = context.params
+  const listTickets = parseDataTicketList()
   const responseData = parseDataTicket()
   const jsdom = require("jsdom");
   const { JSDOM } = jsdom;
   const dom = new JSDOM(headerHtml);
   const wrapper = dom.window.document.querySelector('#root')
+  //get View Data
   Object.keys(responseData).forEach((dataKey)=>{
     let element =  wrapper.querySelector(`[data-key="${dataKey}"]`)
     if(element) {
@@ -27,8 +31,11 @@ export async function getServerSideProps(context) {
         case 'comment':
           let commentData = responseData[dataKey]
       
-          element.outerHTML = commentData.map((comment)=>{
+          element.outerHTML = commentData.map((comment,index)=>{
             let defaultCommentComponent = element.cloneNode(true);
+            if(index !== 0) {
+              defaultCommentComponent.style="visibility:hidden"
+            } 
             Object.keys(comment).forEach((commonKey)=>{
               defaultCommentComponent.querySelector(`[data-key="${commonKey}"]`).innerHTML = comment[commonKey]
               
@@ -40,15 +47,33 @@ export async function getServerSideProps(context) {
           break;          
       }
      
-    }
-    
-   
+    }  
   })
+
+  // List Data 
+  let element =  wrapper.querySelector(`[data-type="list-common"]`)
+  if(element) {
+    element.outerHTML = listTickets.map((ticket)=>{
+      let defaultTicketComponent = element.cloneNode(true);
+        Object.keys(ticket).forEach((ticketKey)=>{
+        let childElement = defaultTicketComponent.querySelector(`[data-key="${ticketKey}"]`)
+        if(childElement) {
+          childElement.innerHTML = ticket[ticketKey]
+        if(ticketKey == 'subject') {
+          childElement.setAttribute('href',`/ticket/${ticket.id}`)
+        }  
+        }
+         
+      })
+      return defaultTicketComponent.outerHTML
+    }).join('')
+  }
+  
   // console.log(dom.window.document.querySelector('[data-ticket="subject"]').textContent); // "Hello world"
   
   return {
     props: {
-     
+      ticketId: slug,
       html:wrapper.outerHTML,
       css:headerStyle,
       script: headerScript
---
+++ pages/ticket/index.js
@@ -0,0 +1,13 @@
+// @refresh reset
+
+
+
+export default function TicketMainPage() {
+  return (
+    <>
+      <a href="/ticket/123">123123</a>
+       ticket main page
+    </>
+  )
+}
+
