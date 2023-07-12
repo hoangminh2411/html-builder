68a9728630621b78663a60b4b9ea5e73cf28d9a9
Tinh Nguyen <tinh258456@gmail.com>
2023-07-12 14:08:18 +0700

    update

--- configs/ticket-form-html.js
+++ configs/ticket-form-html.js
@@ -1,58 +1,59 @@
 export const getTicketForm =(id) =>{
-  const formData = ` <div class="form-field" id="ith43">
-  <label data-mytext="MyText" id="imqb">Subject<span data-mytext="MyText" id="i0s88" draggable="true">*</span></label>
-  <input type="text" name="subject" required id="igt4p"/>
-</div>
-<div class="form-row" id="ieb8">
-  <div class="form-cell" id="iiwn">
-    <div class="form-field" title="Customer" id="i1dmk">
-      <label data-mytext="MyText">Customer</label>
-      <select type="text" id="i05p1" name="customer"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
+  const formData = ` <form class="form-container" id="ih8p">
+  <div class="form-field" id="ith43">
+    <label data-mytext="MyText" id="imqb">Subject<span data-mytext="MyText" id="i0s88" draggable="true">*</span></label>
+    <input type="text" name="subject" required id="igt4p"/>
+  </div>
+  <div class="form-row" id="ieb8">
+    <div class="form-cell" id="iiwn">
+      <div class="form-field" title="Customer" id="i1dmk">
+        <label data-mytext="MyText">Customer</label>
+        <select type="text" id="i05p1" name="customer"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
+      </div>
     </div>
   </div>
-</div>
-<div data-mytext="MyText" id="inkxg">Category
-</div>
-<div class="form-row" id="iqkyl">
-  <div class="form-cell" id="i7lh9">
-    <div class="form-field" id="ie098">
-      <label data-mytext="MyText" id="iyvmj">Issue</label>
-      <input type="text" id="imuwt"/>
+  <div data-mytext="MyText" id="inkxg">Category
+  </div>
+  <div class="form-row" id="iqkyl">
+    <div class="form-cell" id="i7lh9">
+      <div class="form-field" id="ie098">
+        <label data-mytext="MyText" id="iyvmj">Issue</label>
+        <select type="text" id="iqw6l" data-key="issue-write" name="issue"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
+      </div>
+    </div>
+    <div class="form-cell" id="if5jc">
+      <div class="form-field" title="Assigned Rep" id="ibtig">
+        <label data-mytext="MyText" id="iu0zc">Product</label>
+        <select type="text" id="iah8l" name="product" data-key="product-write"><option value="opt1">User 1</option><option value="opt2">user 2</option></select>
+      </div>
     </div>
   </div>
-  <div class="form-cell" id="if5jc">
-    <div class="form-field" title="Assigned Rep" id="ibtig">
-      <label data-mytext="MyText" id="iu0zc">Product</label>
-      <select type="text" id="iah8l" name="assignedUser"><option value="opt1">User 1</option><option value="opt2">user 2</option></select>
+  <div data-mytext="MyText" id="inojh">Classification
+  </div>
+  <div class="form-row" id="iwfe7">
+    <div class="form-cell" id="ibmij">
+      <div class="form-field" id="ihp1a">
+        <label data-mytext="MyText" id="iipvl">Classification</label>
+        <select type="text" id="izosb"><option value="language">Language</option>&lt;</select>
+      </div>
+      <div id="in81k">
+        <select type="text" id="iknvt"><option value="region">Region</option></select>
+      </div>
+    </div>
+    <div class="form-cell" id="i28yu">
+      <div class="form-field" id="ivmum">
+        <label data-mytext="MyText" id="ij1i2">Value</label>
+        <select type="text" id="i2u2m" data-key="language-write" name="language"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
+      </div>
+      <div id="ihe3a">
+        <select type="text" id="in9wy" data-key="region-write" name="region"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
+      </div>
     </div>
   </div>
-</div>
-<div data-mytext="MyText" id="inojh">Classification
-</div>
-<div class="form-row" id="iwfe7">
-  <div class="form-cell" id="ibmij">
-    <div class="form-field" id="ihp1a">
-      <label data-mytext="MyText" id="iipvl">Classification</label>
-      <select type="text" id="izosb"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
-    </div>
-    <div id="in81k">
-      <select type="text" id="iknvt"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
-    </div>
-  </div>
-  <div class="form-cell" id="i28yu">
-    <div class="form-field" id="ivmum">
-      <label data-mytext="MyText" id="ij1i2">Value</label>
-      <select type="text" id="i2u2m"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
-    </div>
-    <div id="ihe3a">
-      <select type="text" id="in9wy"><option value="opt1">Option 1</option><option value="opt2">Option 2</option></select>
-    </div>
-  </div>
-</div>
-<div class="form-field" id="irrur">
-  <label data-mytext="MyText" id="i403p">Content</label>
-  <textarea name="content" required id="ig5to"></textarea>
-</div>`
+  <div class="form-field" id="irrur">
+    <label data-mytext="MyText" id="i403p">Content</label>
+    <textarea name="content" required id="ig5to"></textarea>
+  </div>`
   const cssFormData = `<style>* {
     box-sizing: border-box;
   }
@@ -104,8 +105,22 @@ export const getTicketForm =(id) =>{
     margin-bottom:0px;
     margin-left:0px;
   }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin-top:0px;
+    margin-right:0px;
+    margin-bottom:0px;
+    margin-left:0px;
+  }
   div{
     font-family:Roboto, sans-serif;
+  }
+  .form-cell{
+    min-height:75px;
+    flex-grow:1;
+    flex-basis:100%;
   }
   #ih8p{
     min-height:100vh;
@@ -119,12 +134,19 @@ export const getTicketForm =(id) =>{
     width:100%;
     display:flex;
     position:relative;
+    color:black;
+    margin-top:0px;
+    margin-right:0px;
+    margin-bottom:0px;
+    margin-left:0px;
+    padding-top:10px;
+    padding-right:10px;
+    padding-bottom:10px;
+    padding-left:10px;
     overflow-x:hidden;
     overflow-y:hidden;
-    padding:10px 10px 10px 0px;
+    padding:10px;
     overflow:hidden;
-    color:black;
-    border:0 none 0;
   }
   #ith43{
     display:flex;
@@ -172,7 +194,7 @@ export const getTicketForm =(id) =>{
     transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
     transition-delay:0s, 0s, 0s;
     transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
+    padding:0.375rem 0.75rem;
     fontSize:1rem;
     fontWeight:400;
     lineHeight:1.5;
@@ -183,13 +205,13 @@ export const getTicketForm =(id) =>{
   #i1dmk{
     display:flex;
     flex-direction:column;
-    padding-top:8px;
-    padding-right:8px;
-    padding-bottom:8px;
-    padding-left:8px;
     row-gap:10px;
     column-gap:10px;
     width:100%;
+    padding-top:8px;
+    padding-right:8px;
+    padding-bottom:8px;
+    padding-left:0px;
   }
   #ibtig{
     display:flex;
@@ -200,7 +222,7 @@ export const getTicketForm =(id) =>{
     padding-left:8px;
     row-gap:10px;
     column-gap:10px;
-    width:377.62%;
+    width:100%;
   }
   #i05p1{
     display:block;
@@ -237,7 +259,7 @@ export const getTicketForm =(id) =>{
     transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
     transition-delay:0s, 0s, 0s;
     transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
+    padding:0.375rem 0.75rem;
     fontSize:1rem;
     fontWeight:400;
     lineHeight:1.5;
@@ -280,7 +302,7 @@ export const getTicketForm =(id) =>{
     transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
     transition-delay:0s, 0s, 0s;
     transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
+    padding:0.375rem 0.75rem;
     fontSize:1rem;
     fontWeight:400;
     lineHeight:1.5;
@@ -293,7 +315,8 @@ export const getTicketForm =(id) =>{
     width:100%;
     display:flex;
     position:relative;
-    height:103.667px;
+    height:82.5559px;
+    color:black;
     overflow-x:hidden;
     overflow-y:hidden;
     padding:10px 10px 10px 0px;
@@ -316,49 +339,6 @@ export const getTicketForm =(id) =>{
     column-gap:10px;
     width:100%;
   }
-  #imuwt{
-    display:block;
-    color:#212529;
-    background-color:#FFFFFF;
-    background-clip:padding-box;
-    appearance:none;
-    padding-top:0.375rem;
-    padding-right:0.75rem;
-    padding-bottom:0.375rem;
-    padding-left:0.75rem;
-    border-top-width:1px;
-    border-right-width:1px;
-    border-bottom-width:1px;
-    border-left-width:1px;
-    border-top-style:solid;
-    border-right-style:solid;
-    border-bottom-style:solid;
-    border-left-style:solid;
-    border-top-color:rgb(206, 212, 218);
-    border-right-color:rgb(206, 212, 218);
-    border-bottom-color:rgb(206, 212, 218);
-    border-left-color:rgb(206, 212, 218);
-    border-image-source:initial;
-    border-image-slice:initial;
-    border-image-width:initial;
-    border-image-outset:initial;
-    border-image-repeat:initial;
-    border-top-left-radius:0.25rem;
-    border-top-right-radius:0.25rem;
-    border-bottom-right-radius:0.25rem;
-    border-bottom-left-radius:0.25rem;
-    transition-duration:0.05s, 0.05s, 0.05s;
-    transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
-    transition-delay:0s, 0s, 0s;
-    transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
-    fontSize:1rem;
-    fontWeight:400;
-    lineHeight:1.5;
-    border:1px solid #CED4DA;
-    border-radius:0.25rem;
-    transition:background-color 0.05s ease-in-out, border-color 0.05s ease-in-out, box-shadow 0.05s ease-in-out;
-  }
   #inojh{
     padding-top:10px;
     padding-right:10px;
@@ -375,11 +355,6 @@ export const getTicketForm =(id) =>{
     overflow-y:hidden;
     padding:10px 10px 10px 0px;
     overflow:hidden;
-  }
-  .form-cell{
-    min-height:75px;
-    flex-grow:1;
-    flex-basis:100%;
   }
   #ihp1a{
     display:flex;
@@ -427,7 +402,7 @@ export const getTicketForm =(id) =>{
     transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
     transition-delay:0s, 0s, 0s;
     transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
+    padding:0.375rem 0.75rem;
     fontSize:1rem;
     fontWeight:400;
     lineHeight:1.5;
@@ -477,7 +452,7 @@ export const getTicketForm =(id) =>{
     transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
     transition-delay:0s, 0s, 0s;
     transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
+    padding:0.375rem 0.75rem;
     fontSize:1rem;
     fontWeight:400;
     lineHeight:1.5;
@@ -521,7 +496,7 @@ export const getTicketForm =(id) =>{
     transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
     transition-delay:0s, 0s, 0s;
     transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
+    padding:0.375rem 0.75rem;
     fontSize:1rem;
     fontWeight:400;
     lineHeight:1.5;
@@ -581,7 +556,7 @@ export const getTicketForm =(id) =>{
     transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
     transition-delay:0s, 0s, 0s;
     transition-property:background-color, border-color, box-shadow;
-    padding:0.5rem 0.75rem;
+    padding:0.375rem 0.75rem;
     fontSize:1rem;
     fontWeight:400;
     lineHeight:1.5;
@@ -601,17 +576,61 @@ export const getTicketForm =(id) =>{
     width:100%;
   }
   #iipvl{
-    color:#8c8c8c;
+    color:rgb(140, 140, 140);
   }
   #ij1i2{
-    color:#8c8c8c;
+    color:rgb(140, 140, 140);
   }
   #iyvmj{
-    color:#8c8c8c;
+    color:rgb(140, 140, 140);
   }
   #iu0zc{
-    color:#8c8c8c;
+    color:rgb(140, 140, 140);
   }
+  #iqw6l{
+    display:block;
+    color:#212529;
+    background-color:#FFFFFF;
+    background-clip:padding-box;
+    appearance:none;
+    padding-top:0.375rem;
+    padding-right:0.75rem;
+    padding-bottom:0.375rem;
+    padding-left:0.75rem;
+    border-top-width:1px;
+    border-right-width:1px;
+    border-bottom-width:1px;
+    border-left-width:1px;
+    border-top-style:solid;
+    border-right-style:solid;
+    border-bottom-style:solid;
+    border-left-style:solid;
+    border-top-color:rgb(206, 212, 218);
+    border-right-color:rgb(206, 212, 218);
+    border-bottom-color:rgb(206, 212, 218);
+    border-left-color:rgb(206, 212, 218);
+    border-image-source:initial;
+    border-image-slice:initial;
+    border-image-width:initial;
+    border-image-outset:initial;
+    border-image-repeat:initial;
+    border-top-left-radius:0.25rem;
+    border-top-right-radius:0.25rem;
+    border-bottom-right-radius:0.25rem;
+    border-bottom-left-radius:0.25rem;
+    transition-duration:0.05s, 0.05s, 0.05s;
+    transition-timing-function:ease-in-out, ease-in-out, ease-in-out;
+    transition-delay:0s, 0s, 0s;
+    transition-property:background-color, border-color, box-shadow;
+    padding:0.375rem 0.75rem;
+    fontSize:1rem;
+    fontWeight:400;
+    lineHeight:1.5;
+    border:1px solid #CED4DA;
+    border-radius:0.25rem;
+    transition:background-color 0.05s ease-in-out, border-color 0.05s ease-in-out, box-shadow 0.05s ease-in-out;
+  }
+  
   </style>`
 
   return cssFormData  +  formData
