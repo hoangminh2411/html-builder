2e1ae31c0faa3706ebe20cd297216d7b56a675fa
Tinh Nguyen <tinh258456@gmail.com>
2023-07-14 15:39:22 +0700

    update

--- demo-page/components/autocomplete/script.js
+++ demo-page/components/autocomplete/script.js
@@ -1,20 +1,20 @@
-function autocomplete(autocomplete, arr) {
+function autocomplete(autocomplete, arr,mode) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   console.log(`autocomplete`,autocomplete)
 
-  const inp = autocomplete.querySelector("#myInput")
+  const inp = autocomplete.querySelector(".myInput")
   const container = autocomplete.querySelector(".input-container")
   const value = autocomplete.querySelector(".value-input")
 
 
   var currentFocus;
   /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
+  function init () {
       var autoCompleteList, item, i, val = this.value;
       /*close any already open lists of autocompleted values*/
       closeAllLists();
-      if (!val) { return false;}
+      // if (!val) { return false;}
       currentFocus = -1;
       console.log(`~~~~ val`,val)
       /*create a DIV element that will contain the items (values):*/
@@ -29,7 +29,7 @@ function autocomplete(autocomplete, arr) {
         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
           /*create a DIV element for each matching element:*/
           item = document.createElement("DIV");
-          if (value.value && JSON.parse(value.value).includes(arr[i])) {
+          if (value.value && mode!='single' && JSON.parse(value.value).includes(arr[i])) {
             console.log(`~~~~~~~~~selected`)
             item.className = "selected"
           }
@@ -42,20 +42,25 @@ function autocomplete(autocomplete, arr) {
           item.addEventListener("click", function(e) {
               /*insert the value for the autocomplete text field:*/
           
-              inp.value = ``
               const itemVal= this.getElementsByTagName("input")[0].value;
-              if (!value.value) {
-                
+              if (mode=='single') {
+                value.value=this.getElementsByTagName("input")[0].value
+                inp.value=value.value;
+              }
+              else if (!value.value) {
+                inp.value = ``
                 value.value=JSON.stringify([this.getElementsByTagName("input")[0].value])
               }
               else {
+              inp.value = ``
                let oldVal = JSON.parse(value.value)
                 oldVal.push(this.getElementsByTagName("input")[0].value)
 
                 console.log(`oldVal`,oldVal)
                value.value = JSON.stringify(oldVal);
               }
-              let chip = document.createElement("DIV");
+              console.log(`autoComplete mode`,mode)
+              if (mode !='single') {let chip = document.createElement("DIV");
               chip.className = "chip"
               chip.innerHTML = 
               `<span>${this.getElementsByTagName("input")[0].value}</span><svg class="delete-icon"><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path></svg>`
@@ -71,7 +76,7 @@ function autocomplete(autocomplete, arr) {
                 }
                 value.value = JSON.stringify(curArr);
                 chip.remove();
-              })
+              })}
 
           
               /*close the list of autocompleted values,
@@ -81,8 +86,12 @@ function autocomplete(autocomplete, arr) {
           });
           autoCompleteList.appendChild(item);
         }
-      }
-  });
+      
+  }
+  }
+  inp.addEventListener("input", init);
+  inp.addEventListener("focus", init);
+
   /*execute a function presses a key on the keyboard:*/
   inp.addEventListener("keydown", function(e) {
       var autocompleteList = document.getElementById(this.id + "autocomplete-list");
@@ -108,6 +117,7 @@ function autocomplete(autocomplete, arr) {
         }
       }
   });
+
   function addActive(autocompleteList) {
     /*a function to classify an item as "active":*/
     if (!autocompleteList) return false;
@@ -125,6 +135,7 @@ function autocomplete(autocomplete, arr) {
     }
   }
   function closeAllLists(elmnt) {
+    console.log(`onClose`,elmnt,elmnt != inp)
     /*close all autocomplete lists in the document,
     except the one passed as an argument:*/
     var autocompleteList = document.getElementsByClassName("autocomplete-items");
@@ -137,16 +148,45 @@ function autocomplete(autocomplete, arr) {
 
   /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
+    console.log(`e.target`, e.target , inp ,e.target != inp )
+    if (e.target.className != "myInput") closeAllLists(e.target);
   });
 }
 
-/*An array containing all the country names in the world:*/
-var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
-console.log(`document.getElementsByClassName("autocomplete")[0]`,document.getElementsByClassName("autocomplete")[0])
+
 const allac = document.querySelectorAll(".autocomplete");
 Array.from(allac).forEach((Element) => {
-  autocomplete(Element, countries);
+  const config = Element.querySelector('.autocomplete-config');
+  const options = JSON.parse(config.innerHTML);
+  const mode = config.getAttribute("mode")
+  autocomplete(Element, options,mode? mode : "multiple" );
+})
+
+const formFilter = document.querySelector('#filter');
+formFilter.addEventListener('submit', (e)=> {
+  const form = event.target;
+  const data = new FormData(form);
+  let nTicketData = {};
+  for (const [key, value] of data) {
+    if(['classifications','category','product'].includes(key)) {
+      nTicketData = {
+        ...nTicketData,
+        [key]: JSON.parse(value)
+      };
+    } else {
+      nTicketData = {
+        ...nTicketData,
+        [key]: value
+      };
+    }
+  
+    
+  }
+  console.log("data update",nTicketData)
+  // mutationAdd(newParams);
+  //Prvent the submit event
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
 })
